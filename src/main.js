6c0084e6e6f6a2ffa814fd531ce53a5240872ad0
이상민(lenovo) <lsmin625@gmail.com>
2023-06-05 15:56:25 +0900

    login 설계중

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
 app.use(router)
--- src/views/AdminLogin.vue
+++ src/views/AdminLogin.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        어드민 로그인 화면 입니다.
+    <div class="container text-center mt-5 p-2">
+      <div class="row text-bg-primary fw-bold p-2 m-2">
+        <div class="col">
+          <span class="fs-2"> 워즈 클라우드 서비스 </span>
+        </div>
+      </div>    
+      <div class="container mt-5 d-flex">
+        <div class="input-group mb-1">
+          <span class="d-flex justify-content-end input-group-text w-fix">이메일</span>
+          <input type="email" class="form-control" >
+        </div>
+      </div>
+      <div class="container mt-2 d-flex">
+        <div class="input-group mb-1">
+          <span class="d-flex justify-content-end input-group-text w-fix"> 비밀번호 </span>
+          <input type="password" class="form-control" >
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col d-flex justify-content-end">
+          <button type="button" class="btn btn-primary m-2" >로그인</button>
+          <button type="button" class="btn btn-outline-secondary m-2" >회원가입</button>
+        </div>
+      </div>
     </div>
-</template>
+  </template>
+  
+  <style scoped>
+  .w-fix{
+    width: 110px;
+  }
+  </style>
