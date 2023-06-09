2051d20dda7cc946a7fc2ef764101e740dfa292e
이상민(lenovo) <lsmin625@gmail.com>
2023-06-09 16:38:13 +0900

    테스터 변경

--- src/views/AboutTest.vue
+++ src/views/AboutTest.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="container">
         <div class="mb-3">
-            <label for="titleInput" class="form-label">Modal Title</label>
+            <label for="titleInput" class="form-label">제목</label>
             <input type="text" class="form-control" id="titleInput" v-model="modalTitle">
         </div>
         <div class="mb-3">
-            <label for="messageInput" class="form-label">Modal Message</label>
+            <label for="messageInput" class="form-label">메시지</label>
             <input type="text" class="form-control" id="messageInput" v-model="modalMessage">
         </div>
-        <button @click="openModal" class="btn btn-primary me-2">Open Modal</button>
+        <button @click="openModal" class="btn btn-primary me-2">팝업창</button>
         <MessagePopup />
     </div>
 </template>
@@ -22,23 +22,14 @@ export default {
     },
     data() {
         return {
-            showModal: false,
             modalTitle: '제목',
             modalMessage: '메시지',
         };
     },
     methods: {
         openModal() {
-            this.showModal = true;
+            console.log('openModal');
         },
-        onCancel() {
-            console.log('Cancel clicked');
-            this.showModal = false;
-        },
-        onConfirm() {
-            console.log('Confirm clicked');
-            this.showModal = false;
-        }
     },
 };
 </script>
