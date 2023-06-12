7a4bdba4f8aeafa185a40dbf04d1cebdf368e830
이상민(lenovo) <lsmin625@gmail.com>
2023-06-12 11:10:08 +0900

    제목, 메시지 전달

--- src/components/MessagePopup.vue
+++ src/components/MessagePopup.vue
@@ -10,11 +10,11 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">{{ title }}</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    ...
+                    {{ message }}
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -34,10 +34,14 @@ export default {
                 id: COMPONENT,
                 target: "#" + COMPONENT,
             },
+            title: null,
+            message: null,
         };
     },
     methods: {
-        show() {
+        show(title, message) {
+            this.title = title;
+            this.message = message;
             this.$refs[this.component.id].click();
         },
     },
